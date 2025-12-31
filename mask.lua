ea38f819d8302cef4494108d3abc49f9692b0a6f
dbugrbx <dbugdev@outlook.com>
2025-12-30 23:01:47 -0500

    Create mask.lua

---
+++ mask.lua
@@ -0,0 +1,35 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
+local Window = Rayfield:CreateWindow({
+   	Name = "Mask",
+   	Icon = 0,
+   	LoadingTitle = "Mask,
+   	LoadingSubtitle = "Configuring Assets...",
+   	ShowText = "Mask",
+   	Theme = "Default",
+   	ToggleUIKeybind = "M",
+   	DisableRayfieldPrompts = true,
+   	DisableBuildWarnings = false,
+   	ConfigurationSaving = {
+      	Enabled = true,
+      	FolderName = nil,
+      	FileName = "Mask"
+   	},
+   	Discord = {
+      	Enabled = true,
+      	Invite = "https://discord.gg/xgkcgArxKT",
+      	RememberJoins = true
+   	},
+   	KeySystem = true,
+   	KeySettings = {
+      	Title = "Mask Access Key",
+      	Subtitle = "Access Key is required to continue.",
+      	Note = "Join the Inverted HQ Discord Server for the Access Key.",
+      	FileName = "AccessKey",
+      	SaveKey = false,
+      	GrabKeyFromSite = false,
+      	Key = {"januarysnow2026"}
+   	}
+})
+Rayfield:LoadConfiguration()
